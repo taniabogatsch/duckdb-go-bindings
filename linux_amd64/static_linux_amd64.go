8abd8b0591dc535700767a00f06ac13e2000f9f5
taniabogatsch <44262898+taniabogatsch@users.noreply.github.com>
2025-02-12 13:12:51 +0100

    add correct ldflags

--- linux_amd64/static_linux_amd64.go
+++ linux_amd64/static_linux_amd64.go
@@ -3,7 +3,7 @@ package duckdb_go_bindings
 /*
 #cgo CPPFLAGS: -DDUCKDB_STATIC_BUILD
 #cgo LDFLAGS: -lduckdb
-#cgo LDFLAGS: -lc++ -L${SRCDIR}/libs
+#cgo LDFLAGS: -lstdc++ -lm -ldl -L${SRCDIR}/libs
 #include <duckdb.h>
 */
 import "C"
--- windows_amd64/static_windows_amd64.go
+++ windows_amd64/static_windows_amd64.go
@@ -3,7 +3,7 @@ package duckdb_go_bindings
 /*
 #cgo CPPFLAGS: -DDUCKDB_STATIC_BUILD
 #cgo LDFLAGS: -lduckdb
-#cgo LDFLAGS: -lc++ -L${SRCDIR}/libs
+#cgo LDFLAGS: -lws2_32 -lwsock32 -lrstrtmgr -lstdc++ -lm --static -L${SRCDIR}/libs
 #include <duckdb.h>
 */
 import "C"
