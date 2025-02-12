4572cf88c7a79f0f2081f6572e402dbec8e2127c
taniabogatsch <44262898+taniabogatsch@users.noreply.github.com>
2025-02-12 15:40:17 +0100

    update build file

--- windows_amd64/static_windows_amd64.go
+++ windows_amd64/static_windows_amd64.go
@@ -2,8 +2,7 @@ package duckdb_go_bindings
 
 /*
 #cgo CPPFLAGS: -DDUCKDB_STATIC_BUILD
-#cgo LDFLAGS: -lduckdb
-#cgo LDFLAGS: -lws2_32 -lwsock32 -lrstrtmgr -lstdc++ -lm --static -L${SRCDIR}/libs
+#cgo LDFLAGS: -lws2_32 -lwsock32 -lrstrtmgr -lstdc++ -lm --static -lduckdb_part1 -lduckdb_part2 -L${SRCDIR}/libs
 #include <duckdb.h>
 */
 import "C"
