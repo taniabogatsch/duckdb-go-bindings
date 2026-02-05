088016269270024982838af5a0fb4344e28fba25
taniabogatsch <44262898+taniabogatsch@users.noreply.github.com>
2026-02-05 14:46:17 +0100

    check in v1.5 pre-release (2323327f46a9e0116fc9830f0fa92c13e2d3ce5b)

--- lib/darwin-amd64/prebuilt.go
+++ lib/darwin-amd64/prebuilt.go
@@ -4,7 +4,7 @@ package duckdb_go_bindings_platform
 
 /*
 #cgo CPPFLAGS: -I${SRCDIR} -DDUCKDB_STATIC_BUILD
-#cgo LDFLAGS: -lduckdb_static -lautocomplete_extension -lcore_functions_extension -licu_extension -ljson_extension -lparquet_extension -ltpcds_extension -ltpch_extension -lduckdb_fastpforlib -lduckdb_fmt -lduckdb_fsst -lduckdb_hyperloglog -lduckdb_mbedtls -lduckdb_miniz -lduckdb_pg_query -lduckdb_re2 -lduckdb_skiplistlib -lduckdb_utf8proc -lduckdb_yyjson -lduckdb_zstd -lc++ -L${SRCDIR}
+#cgo LDFLAGS: -lduckdb_static -lautocomplete_extension -lcore_functions_extension -licu_extension -ljson_extension -lparquet_extension -ltpcds_extension -ltpch_extension -lduckdb_fastpforlib -lduckdb_fmt -lduckdb_fsst -lduckdb_hyperloglog -lduckdb_mbedtls -lduckdb_miniz -lduckdb_pg_query -lduckdb_re2 -lduckdb_skiplistlib -lduckdb_utf8proc -lduckdb_yyjson -lduckdb_zstd -lduckdb_generated_extension_loader -lc++ -L${SRCDIR}
 #include <duckdb.h>
 */
 import "C"
